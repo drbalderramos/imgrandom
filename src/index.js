05cfdf998c758cc12a14849a75545a7910d111f4
drbalderramos <dbalderramos@gmail.com>
2021-03-24 23:45:37 -0600

    Proyecto - Lazy Loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from './lazy'
 
-console.log('Happy hacking :)')
+const MIN = 1;
+const MAX = 100;
+const RANDOM = () => Math.floor(Math.random() * (MAX - MIN)) + MIN;
+
+const createImageNode = () => {
+    const container = document.createElement('div');
+    container.className = 'p-4';
+
+    const img = document.createElement('img');
+    img.className = 'mx-auto rounded-md bg-gray-300';
+    img.style = 'width: 320px'
+    img.dataset.src = `https://picsum.photos/500/500?random=${RANDOM()}.jpg`;
+
+    container.append(img);
+
+    return container;
+}
+
+const APP = document.querySelector('#app');
+
+const addButton = document.querySelector('#addImages');
+const actionAddImg = () => {
+    const newImage = createImageNode();
+    APP.append(newImage);
+    registerImage(newImage);
+}
+addButton.addEventListener('click', actionAddImg);
+
+const clean = document.querySelector('#clean');
+clean.addEventListener('click', () => APP.innerHTML="")
